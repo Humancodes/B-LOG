ebcd5c38bc673f4931eeb097b018458f3c7624dc
Humancodes <vyvivek2018@gmail.com>
2024-02-19 18:00:00 +0530

    Blogging website

---
+++ src/app/layout.js
@@ -0,0 +1,28 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: {
+    default: "B-LOG",
+    template: "%s | B-LOG",
+  },
+  description: "A Blogging Website",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="container">
+          <Navbar />
+          {children}
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import styles from "./home.module.css";
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>The Future Bloggers.</h1>
+        <p className={styles.desc}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg} />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
